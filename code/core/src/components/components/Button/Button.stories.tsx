cdda2b55134e5786b006c949a8645eb24a3de29e
Kasper Peulen <kasperpeulen@gmail.com>
2025-01-03 11:54:14 +0100

    Ignore eslint

--- code/core/src/components/components/Button/Button.stories.tsx
+++ code/core/src/components/components/Button/Button.stories.tsx
@@ -7,14 +7,12 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { config } from '../../../../../.storybook/preview';
 import { Button } from './Button';
 
+// eslint-disable-next-line storybook/default-exports
 const meta = config.meta({
   title: 'Button',
   component: Button,
   args: { children: 'Button' },
 });
-
-export default meta;
-type Story = StoryObj<typeof meta>;
 
 const Stack = ({ children }: { children: ReactNode }) => (
   <div style={{ display: 'flex', flexDirection: 'column', gap: '1rem' }}>{children}</div>
@@ -24,9 +22,9 @@ const Row = ({ children }: { children: ReactNode }) => (
   <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>{children}</div>
 );
 
-export const Base: Story = {};
+export const Base = meta.story({});
 
-export const Variants: Story = {
+export const Variants = meta.story({
   render: (args) => (
     <Stack>
       <Row>
@@ -64,125 +62,125 @@ export const Variants: Story = {
       </Row>
     </Stack>
   ),
-};
+});
 
-export const Active: Story = {
-  args: {
-    active: true,
-    children: (
-      <>
-        <FaceHappyIcon />
-        Button
-      </>
-    ),
-  },
-  render: (args) => (
-    <Row>
-      <Button variant="solid" {...args} />
-      <Button variant="outline" {...args} />
-      <Button variant="ghost" {...args} />
-    </Row>
-  ),
-};
-
-export const WithIcon: Story = {
-  args: {
-    children: (
-      <>
-        <FaceHappyIcon />
-        Button
-      </>
-    ),
-  },
-  render: (args) => (
-    <Row>
-      <Button variant="solid" {...args} />
-      <Button variant="outline" {...args} />
-      <Button variant="ghost" {...args} />
-    </Row>
-  ),
-};
-
-export const IconOnly: Story = {
-  args: {
-    children: <FaceHappyIcon />,
-    padding: 'small',
-  },
-  render: (args) => (
-    <Row>
-      <Button variant="solid" {...args} />
-      <Button variant="outline" {...args} />
-      <Button variant="ghost" {...args} />
-    </Row>
-  ),
-};
-
-export const Sizes: Story = {
-  render: () => (
-    <Row>
-      <Button size="small">Small Button</Button>
-      <Button size="medium">Medium Button</Button>
-    </Row>
-  ),
-};
-
-export const Disabled: Story = {
-  args: {
-    disabled: true,
-    children: 'Disabled Button',
-  },
-};
-
-export const WithHref: Story = {
-  render: () => (
-    <Row>
-      <Button onClick={() => console.log('Hello')}>I am a button using onClick</Button>
-      <Button asChild>
-        <a href="https://storybook.js.org/">I am an anchor using Href</a>
-      </Button>
-    </Row>
-  ),
-};
-
-export const Animated: Story = {
-  args: {
-    variant: 'outline',
-  },
-  render: (args) => (
-    <Stack>
-      <Row>
-        <Button animation="glow" {...args}>
-          Button
-        </Button>
-        <Button animation="jiggle" {...args}>
-          Button
-        </Button>
-        <Button animation="rotate360" {...args}>
-          Button
-        </Button>
-      </Row>
-      <Row>
-        <Button animation="glow" {...args}>
-          <FaceHappyIcon /> Button
-        </Button>
-        <Button animation="jiggle" {...args}>
-          <FaceHappyIcon /> Button
-        </Button>
-        <Button animation="rotate360" {...args}>
-          <FaceHappyIcon /> Button
-        </Button>
-      </Row>
-      <Row>
-        <Button animation="glow" padding="small" {...args}>
-          <FaceHappyIcon />
-        </Button>
-        <Button animation="jiggle" padding="small" {...args}>
-          <FaceHappyIcon />
-        </Button>
-        <Button animation="rotate360" padding="small" {...args}>
-          <FaceHappyIcon />
-        </Button>
-      </Row>
-    </Stack>
-  ),
-};
+// export const Active: Story = {
+//   args: {
+//     active: true,
+//     children: (
+//       <>
+//         <FaceHappyIcon />
+//         Button
+//       </>
+//     ),
+//   },
+//   render: (args) => (
+//     <Row>
+//       <Button variant="solid" {...args} />
+//       <Button variant="outline" {...args} />
+//       <Button variant="ghost" {...args} />
+//     </Row>
+//   ),
+// };
+//
+// export const WithIcon: Story = {
+//   args: {
+//     children: (
+//       <>
+//         <FaceHappyIcon />
+//         Button
+//       </>
+//     ),
+//   },
+//   render: (args) => (
+//     <Row>
+//       <Button variant="solid" {...args} />
+//       <Button variant="outline" {...args} />
+//       <Button variant="ghost" {...args} />
+//     </Row>
+//   ),
+// };
+//
+// export const IconOnly: Story = {
+//   args: {
+//     children: <FaceHappyIcon />,
+//     padding: 'small',
+//   },
+//   render: (args) => (
+//     <Row>
+//       <Button variant="solid" {...args} />
+//       <Button variant="outline" {...args} />
+//       <Button variant="ghost" {...args} />
+//     </Row>
+//   ),
+// };
+//
+// export const Sizes: Story = {
+//   render: () => (
+//     <Row>
+//       <Button size="small">Small Button</Button>
+//       <Button size="medium">Medium Button</Button>
+//     </Row>
+//   ),
+// };
+//
+// export const Disabled: Story = {
+//   args: {
+//     disabled: true,
+//     children: 'Disabled Button',
+//   },
+// };
+//
+// export const WithHref: Story = {
+//   render: () => (
+//     <Row>
+//       <Button onClick={() => console.log('Hello')}>I am a button using onClick</Button>
+//       <Button asChild>
+//         <a href="https://storybook.js.org/">I am an anchor using Href</a>
+//       </Button>
+//     </Row>
+//   ),
+// };
+//
+// export const Animated: Story = {
+//   args: {
+//     variant: 'outline',
+//   },
+//   render: (args) => (
+//     <Stack>
+//       <Row>
+//         <Button animation="glow" {...args}>
+//           Button
+//         </Button>
+//         <Button animation="jiggle" {...args}>
+//           Button
+//         </Button>
+//         <Button animation="rotate360" {...args}>
+//           Button
+//         </Button>
+//       </Row>
+//       <Row>
+//         <Button animation="glow" {...args}>
+//           <FaceHappyIcon /> Button
+//         </Button>
+//         <Button animation="jiggle" {...args}>
+//           <FaceHappyIcon /> Button
+//         </Button>
+//         <Button animation="rotate360" {...args}>
+//           <FaceHappyIcon /> Button
+//         </Button>
+//       </Row>
+//       <Row>
+//         <Button animation="glow" padding="small" {...args}>
+//           <FaceHappyIcon />
+//         </Button>
+//         <Button animation="jiggle" padding="small" {...args}>
+//           <FaceHappyIcon />
+//         </Button>
+//         <Button animation="rotate360" padding="small" {...args}>
+//           <FaceHappyIcon />
+//         </Button>
+//       </Row>
+//     </Stack>
+//   ),
+// };
