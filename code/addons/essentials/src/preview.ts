49353cbab659f361431e74cecb89fa3767cce865
Kasper Peulen <kasperpeulen@gmail.com>
2025-01-08 14:42:47 +0100

    Fix addon-essentials

--- code/addons/essentials/src/preview.ts
+++ code/addons/essentials/src/preview.ts
@@ -1,12 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck
+
 /* eslint-disable import/namespace */
 import { composeConfigs } from 'storybook/internal/preview-api';
 
-import * as actions from './actions/preview';
-import * as backgrounds from './backgrounds/preview';
-import * as docs from './docs/preview';
-import * as highlight from './highlight/preview';
-import * as measure from './measure/preview';
-import * as outline from './outline/preview';
-import * as viewport from './viewport/preview';
+import * as actions from '@storybook/addon-actions/preview';
+import * as backgrounds from '@storybook/addon-backgrounds/preview';
+import * as docs from '@storybook/addon-docs/preview';
+import * as highlight from '@storybook/addon-highlight/preview';
+import * as measure from '@storybook/addon-measure/preview';
+import * as outline from '@storybook/addon-outline/preview';
+import * as viewport from '@storybook/addon-viewport/preview';
 
 export default composeConfigs([actions, docs, backgrounds, viewport, measure, outline, highlight]);
