234c8da9c2d29de39e7fca41ee6915b93593b16b
Norbert de Langen <ndelangen@me.com>
2025-01-09 18:38:56 +0100

    alt solution

--- code/lib/create-storybook/src/ink/Demo.stories.tsx
+++ code/lib/create-storybook/src/ink/Demo.stories.tsx
@@ -3,21 +3,12 @@ import React from 'react';
 
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Terminal } from '@xterm/xterm';
+// import { Terminal } from '@xterm/xterm';
 import '@xterm/xterm/css/xterm.css';
 import EventEmitter from 'events';
-import { render } from 'ink';
 
+// import { render } from 'ink';
 import { Demo } from './Demo';
-
-if (Intl.Segmenter === undefined) {
-  // @ts-expect-error (TODO remove when chromatic capture has updated their firefox version)
-  Intl.Segmenter = class DummySegmenter {
-    segment() {
-      return [''];
-    }
-  };
-}
 
 interface Stream extends EventEmitter {
   output: string;
@@ -36,23 +27,6 @@ declare global {
   // eslint-disable-next-line no-var, @typescript-eslint/naming-convention
   var __XTERM_INSTANCES__: any;
 }
-
-const customViewports = {
-  small: {
-    name: 'Small',
-    styles: {
-      width: '380px',
-      height: '500px',
-    },
-  },
-  large: {
-    name: 'Large',
-    styles: {
-      width: '880px',
-      height: '500px',
-    },
-  },
-};
 
 const meta = {
   globals: {
@@ -75,52 +49,44 @@ const meta = {
         globalThis.__XTERM_INSTANCES__[id].dispose();
       }
 
-      const term = new Terminal({ convertEol: true, disableStdin: false, cols: 120, rows: 26 });
-
-      globalThis.__XTERM_INSTANCES__[id] = term;
-
-      const createStdout = (columns?: number): Stream => {
-        const stdout = new EventEmitter() as Stream;
-        stdout.columns = columns ?? 120;
-        stdout.rows = 26;
-        stdout.isTTY = true;
-        stdout.write = (str: string) => {
-          term.write(str.trim());
-        };
-        stdout.setEncoding = () => {};
-        stdout.setRawMode = () => {};
-        // @ts-expect-error (TODO)
-        stdout.ref = () => {};
-        // @ts-expect-error (TODO)
-        stdout.unref = () => {};
-        stdout.resume = () => {};
-        stdout.pause = () => {};
-        return stdout;
-      };
-
-      const stdout = createStdout() as any;
-      const stdin = createStdout() as any;
-
-      term.onData((data) => {
-        stdin.emit('data', data);
-      });
+      if (navigator.userAgent.includes('Firefox')) {
+        return <div>Not supported in Firefox</div>;
+      }
 
       const element = document.getElementById(`terminal--${id}`);
 
-      if (element) {
-        element.innerHTML = '';
-        term.open(document.getElementById(`terminal--${id}`) as HTMLElement);
-        term.focus();
-        render(React.createElement(Story, {}), {
-          stdout: stdout,
-          stderr: stdout,
-          stdin,
-          debug: false,
-          patchConsole: false,
-          isTTY: false,
+      Promise.all([import('ink'), import('@xterm/xterm')]).then(([{ render }, { Terminal }]) => {
+        const term = new Terminal({ convertEol: true, disableStdin: false, cols: 120, rows: 26 });
+        globalThis.__XTERM_INSTANCES__[id] = term;
+
+        const createStdout = (columns?: number): Stream => {
+          const stdout = new EventEmitter() as Stream;
+          stdout.columns = columns ?? 120;
+          stdout.rows = 26;
+          stdout.isTTY = true;
+          stdout.write = (str: string) => {
+            term.write(str.trim());
+          };
+          stdout.setEncoding = () => {};
+          stdout.setRawMode = () => {};
+          // @ts-expect-error (TODO)
+          stdout.ref = () => {};
+          // @ts-expect-error (TODO)
+          stdout.unref = () => {};
+          stdout.resume = () => {};
+          stdout.pause = () => {};
+          return stdout;
+        };
+
+        const stdout = createStdout() as any;
+        const stdin = createStdout() as any;
+
+        term.onData((data) => {
+          stdin.emit('data', data);
         });
-      } else {
-        setTimeout(() => {
+
+        if (element) {
+          element.innerHTML = '';
           term.open(document.getElementById(`terminal--${id}`) as HTMLElement);
           term.focus();
           render(React.createElement(Story, {}), {
@@ -131,8 +97,21 @@ const meta = {
             patchConsole: false,
             isTTY: false,
           });
-        }, 100);
-      }
+        } else {
+          setTimeout(() => {
+            term.open(document.getElementById(`terminal--${id}`) as HTMLElement);
+            term.focus();
+            render(React.createElement(Story, {}), {
+              stdout: stdout,
+              stderr: stdout,
+              stdin,
+              debug: false,
+              patchConsole: false,
+              isTTY: false,
+            });
+          }, 100);
+        }
+      });
 
       return <div id={`terminal--${id}`}></div>;
     },
