dd7fd8435d00cdd66b62da568aa708d4d047fe77
Yann Braga <yannbf@gmail.com>
2025-01-10 16:22:01 +0100

    add CSF4 support in save from controls feature
    closes https://github.com/storybookjs/storybook/issues/30244

---
+++ code/core/src/core-server/utils/save-story/mocks/csf4-variances.stories.tsx
@@ -0,0 +1,14 @@
+import { config } from '#.storybook/preview';
+
+const meta = config.meta({
+  title: 'MyComponent',
+  args: {
+    initial: 'foo',
+  },
+});
+export const Empty = meta.story({});
+export const WithArgs = meta.story({
+  args: {
+    foo: 'bar',
+  },
+});
