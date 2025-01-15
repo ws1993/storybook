2322fb0bd3665ef84195023933d26c223ce63b6f
Steve Dodier-Lazaro <sidnioulz@gmail.com>
2025-01-15 10:47:18 +0100

    Blocks: Ensure Subheading slugs heading IDs to remove spaces

--- code/lib/blocks/src/blocks/Subheading.tsx
+++ code/lib/blocks/src/blocks/Subheading.tsx
@@ -3,14 +3,18 @@ import React from 'react';
 
 import { H3 } from 'storybook/internal/components';
 
+import GithubSlugger from 'github-slugger';
+
 import type { HeadingProps } from './Heading';
 import { HeaderMdx } from './mdx';
+
+const slugs = new GithubSlugger();
 
 export const Subheading: FC<PropsWithChildren<HeadingProps>> = ({ children, disableAnchor }) => {
   if (disableAnchor || typeof children !== 'string') {
     return <H3>{children}</H3>;
   }
-  const tagID = globalThis.encodeURIComponent(children.toLowerCase());
+  const tagID = slugs.slug(children.toLowerCase());
   return (
     <HeaderMdx as="h3" id={tagID}>
       {children}
