5ac85efd5cece91d01db7e9a0f0d25536885a181
Yann Braga <yannbf@gmail.com>
2025-01-17 13:07:42 +0100

    rename the import and function

--- code/frameworks/svelte-webpack5/src/csf-factory/defineMainConfig.ts
+++ code/frameworks/svelte-webpack5/src/csf-factory/defineMainConfig.ts
@@ -2,6 +2,6 @@ import { defineConfig as commonDefineConfig } from 'storybook/internal/common';
 
 import type { StorybookConfig } from '../types';
 
-export function defineConfig(config: StorybookConfig) {
+export function defineMain(config: StorybookConfig) {
   return commonDefineConfig<StorybookConfig>(config);
 }
