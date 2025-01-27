7967016e4ff8b9796bc6b91a978dba40cc731da1
Yann Braga <yannbf@gmail.com>
2025-01-27 17:24:42 +0100

    add /preview to addon-essentials

--- code/addons/essentials/src/index.ts
+++ code/addons/essentials/src/index.ts
@@ -1,22 +1,5 @@
-import { composeConfigs, definePreview } from 'storybook/internal/preview-api';
+import { definePreview } from 'storybook/internal/preview-api';
 
-import actionsAddon from '@storybook/addon-actions';
-import backgroundsAddon from '@storybook/addon-backgrounds';
-import docsAddon from '@storybook/addon-docs';
-import highlightAddon from '@storybook/addon-highlight';
-import measureAddon from '@storybook/addon-measure';
-import outlineAddon from '@storybook/addon-outline';
-import viewportAddon from '@storybook/addon-viewport';
+import addonAnnotations from './preview';
 
-export default () =>
-  definePreview(
-    composeConfigs([
-      actionsAddon(),
-      docsAddon(),
-      backgroundsAddon(),
-      viewportAddon(),
-      measureAddon(),
-      outlineAddon(),
-      highlightAddon(),
-    ])
-  );
+export default () => definePreview(addonAnnotations);
---
+++ code/addons/essentials/src/preview.ts
@@ -0,0 +1,19 @@
+import { composeConfigs } from 'storybook/internal/preview-api';
+
+import actionsAddon from '@storybook/addon-actions';
+import backgroundsAddon from '@storybook/addon-backgrounds';
+import docsAddon from '@storybook/addon-docs';
+import highlightAddon from '@storybook/addon-highlight';
+import measureAddon from '@storybook/addon-measure';
+import outlineAddon from '@storybook/addon-outline';
+import viewportAddon from '@storybook/addon-viewport';
+
+export default composeConfigs([
+  actionsAddon(),
+  docsAddon(),
+  backgroundsAddon(),
+  viewportAddon(),
+  measureAddon(),
+  outlineAddon(),
+  highlightAddon(),
+]);
