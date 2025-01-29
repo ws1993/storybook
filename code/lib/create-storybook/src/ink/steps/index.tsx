46771c5242b93f828df0e6fc442626357f356645
Norbert de Langen <ndelangen@me.com>
2025-01-29 12:20:06 +0100

    cleanup

--- code/lib/create-storybook/src/ink/steps/index.tsx
+++ code/lib/create-storybook/src/ink/steps/index.tsx
@@ -1,29 +1,30 @@
 import { type Dispatch, type FC } from 'react';
 
-import type { Input } from '../app';
+// import type { Input } from '../app';
 import { getKeys } from '../utils/getKeys';
-import { CHECK } from './Check';
-import { DIRECTORY } from './Directory';
-import { FEATURES } from './Features';
-import { FRAMEWORK } from './Framework';
-import { GIT } from './Git';
-import { INSTALL } from './Install';
-import { INTENTS } from './Intents';
-import { RUN } from './Run';
-import { SANDBOX } from './Sandbox';
-import { VERSION } from './Version';
+
+// import { CHECK } from './Check';
+// import { DIRECTORY } from './Directory';
+// import { FEATURES } from './Features';
+// import { FRAMEWORK } from './Framework';
+// import { GIT } from './Git';
+// import { INSTALL } from './Install';
+// import { INTENTS } from './Intents';
+// import { RUN } from './Run';
+// import { SANDBOX } from './Sandbox';
+// import { VERSION } from './Version';
 
 export const steps = {
-  GIT,
-  VERSION,
-  DIRECTORY,
-  FRAMEWORK,
-  INTENTS,
-  FEATURES,
-  CHECK,
-  INSTALL,
-  SANDBOX,
-  RUN,
+  // GIT,
+  // VERSION,
+  // DIRECTORY,
+  // FRAMEWORK,
+  // INTENTS,
+  // FEATURES,
+  // CHECK,
+  // INSTALL,
+  // SANDBOX,
+  // RUN,
 } satisfies Record<string, FC<{ state: State; dispatch: Dispatch<Action> }>>;
 const keys = getKeys(steps);
 
@@ -84,56 +85,56 @@ export type Action =
       payload: { code: number; reasons: string[] };
     };
 
-export type State = Omit<Input, 'width' | 'height'> & {
+export type State = Omit<{}, 'width' | 'height'> & {
   step: keyof typeof steps;
   directory: string;
   version: 'latest' | 'outdated' | undefined;
 };
-export function reducer(state: State, action: Action): State {
-  const current = keys.indexOf(state.step);
-  const next = current === keys.length - 1 ? keys[current] : keys[current + 1];
+// export function reducer(state: State, action: Action): State {
+//   const current = keys.indexOf(state.step);
+//   const next = current === keys.length - 1 ? keys[current] : keys[current + 1];
 
-  switch (action.type) {
-    case ACTIONS.NEXT:
-      return { ...state, step: next };
-    case ACTIONS.IGNORE_GIT:
-      return {
-        ...state,
-        ignoreGitNotClean: true,
-        step: next,
-      };
-    case ACTIONS.IGNORE_VERSION:
-      return {
-        ...state,
-        ignoreVersion: true,
-        version: action.payload.value,
-        step: next,
-      };
-    case ACTIONS.DIRECTORY:
-      return {
-        ...state,
-        directory: action.payload.path,
-        step: next,
-      };
-    case ACTIONS.SET_FRAMEWORK:
-      return { ...state, framework: action.payload.id };
-    case ACTIONS.FRAMEWORK:
-      return { ...state, framework: action.payload.id, step: next };
-    case ACTIONS.INTENTS:
-      return { ...state, intents: action.payload.list, step: next };
-    case ACTIONS.IGNORE_TEST_INTENT:
-      return { ...state, intents: (state.intents || []).filter((i) => i !== 'test') };
-    case ACTIONS.FEATURES:
-      return {
-        ...state,
-        features: action.payload.list,
-        step: next,
-      };
-    case ACTIONS.INSTALL:
-      return { ...state, install: action.payload.value, step: next };
-    case ACTIONS.EXIT:
-      process.exit(0);
-    default:
-      return state;
-  }
-}
+//   switch (action.type) {
+//     case ACTIONS.NEXT:
+//       return { ...state, step: next };
+//     case ACTIONS.IGNORE_GIT:
+//       return {
+//         ...state,
+//         ignoreGitNotClean: true,
+//         step: next,
+//       };
+//     case ACTIONS.IGNORE_VERSION:
+//       return {
+//         ...state,
+//         ignoreVersion: true,
+//         version: action.payload.value,
+//         step: next,
+//       };
+//     case ACTIONS.DIRECTORY:
+//       return {
+//         ...state,
+//         directory: action.payload.path,
+//         step: next,
+//       };
+//     case ACTIONS.SET_FRAMEWORK:
+//       return { ...state, framework: action.payload.id };
+//     case ACTIONS.FRAMEWORK:
+//       return { ...state, framework: action.payload.id, step: next };
+//     case ACTIONS.INTENTS:
+//       return { ...state, intents: action.payload.list, step: next };
+//     case ACTIONS.IGNORE_TEST_INTENT:
+//       return { ...state, intents: (state.intents || []).filter((i) => i !== 'test') };
+//     case ACTIONS.FEATURES:
+//       return {
+//         ...state,
+//         features: action.payload.list,
+//         step: next,
+//       };
+//     case ACTIONS.INSTALL:
+//       return { ...state, install: action.payload.value, step: next };
+//     case ACTIONS.EXIT:
+//       process.exit(0);
+//     default:
+//       return state;
+//   }
+// }
