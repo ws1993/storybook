cecfd850f3f073aed1d8b06b2cb1c1e240827839
Kasper Peulen <kasperpeulen@gmail.com>
2025-01-31 10:18:25 +0100

    Fix eslint

--- code/.eslintrc.js
+++ code/.eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
       'error',
       { devDependencies: true, peerDependencies: true },
     ],
-    'no-underscore-dangle': 'off',
     'import/no-unresolved': 'off', // covered by typescript
     'eslint-comments/disable-enable-pair': ['error', { allowWholeFile: true }],
     'eslint-comments/no-unused-disable': 'error',
