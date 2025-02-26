00e42a307de5331b87e5b8084d9d000075df1cda
Norbert de Langen <ndelangen@me.com>
2025-02-26 16:26:22 +0100

    accept change by dedupe

--- code/core/src/manager/globals/exports.ts
+++ code/core/src/manager/globals/exports.ts
@@ -12,7 +12,6 @@ export default {
     'StrictMode',
     'Suspense',
     '__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED',
-    'act',
     'cloneElement',
     'createContext',
     'createElement',
